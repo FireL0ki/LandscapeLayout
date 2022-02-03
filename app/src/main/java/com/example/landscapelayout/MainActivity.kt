e2a47793d7a91039fcd52ca1a0e0be369af31281
muell <muell758@d.umn.edu>
2022-02-02 18:34:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/landscapelayout/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.landscapelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var seekBar: SeekBar
+    private lateinit var seekBarLabel: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekBar = findViewById(R.id.seek_bar)
+        seekBarLabel = findViewById(R.id.seek_bar_label)
+
+        val initialProgress = seekBar.progress
+        updateLabel(initialProgress)
+
+        // TODO add listener to update label as seekbar changes
+        // seekbar change listener
+
+        seekBar.setOnSeekBarChangeListener( object: SeekBar.OnSeekBarChangeListener {
+            // p0 -- seekbarComponent, p1 -- progress, p2 -- fromUser
+            override fun onProgressChanged(seebarComponent: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateLabel(progress)
+            }
+
+            // need these, but don't need to have anything in them
+            // our app doesn't need to do anything specific when use starts/stops using seekbar
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+    }
+
+    private fun updateLabel(progress: Int) {
+        seekBarLabel.text = getString(R.string.seekbar_value_message, progress)
+    }
+
+
+}
